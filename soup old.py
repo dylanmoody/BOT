5f9b224c577f882748e72438161aafdbbcf00f30
mpcortes6 <cortea3@rpi.edu>
2020-03-31 15:49:44 -0700

    Added product searching functionality and removed electron quick start wqþwq

--- soup old.py
+++ soup old.py
@@ -11,48 +11,13 @@ shipping_ids = ["checkout_email" ,"checkout_shipping_address_first_name", "check
 shipping_values = ["bobob@bob.com","bob", "BOBOB", "68 Road Lane", "", "Troy", "New York", "12180", "1234567789"]
 
 #set proxy/proxies
-proxies={'http': '154.3.37.178:1337:tf6j8eu:t4kjq',}
+proxies={'http': 'my proxy',}
 brow.session.proxies = proxies
 
-# When using variants import variant link here 
-# # use brow to open link
-# brow.open("https://shopnicekicks.com/cart/10638898049:1")
-# # check url
-# print(brow.get_url())
-
-
 # use brow to open link
-brow.open("https://shopnicekicks.com")
-
-# search current page for new page links
-for link in brow.links(url_regex=r"accessories"):
-	r = brow.follow_link(link)
-	print(brow.get_url())
-	break
-
-# search current page for new page links
-for link in brow.links(url_regex=r"nice-kicks-truck-pin"):
-	r = brow.follow_link(link)
-	print(brow.get_url())
-	break
-
-
-# select form
-brow.select_form(nr=1)
-# submit form (add to cart)
-brow.submit_selected()
-# # check url
+brow.open("https://shopnicekicks.com/cart/10638898049:1")
+# check url
 print(brow.get_url())
-
-
-# select form
-brow.select_form(nr=0)
-# submit form (go to checkout page)
-brow.submit_selected()
-# # check url
-print(brow.get_url())
-
-
 # select form
 brow.select_form(nr=0)
 
@@ -72,6 +37,6 @@ brow.submit_selected()
 print(brow.get_url())
 
 
-# brow.select_form(nr=2)
-# # brow.get_current_form().print_summary()
+brow.select_form(nr=2)
+# brow.get_current_form().print_summary()
 
